1f70d3a89687a2fa40597d57df089040497ab671
Romulo P T Mota <romuloptmota@gmail.com>
2023-02-07 13:51:53 -0300

    Criado views do cadastro, ainda para teste

--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from .forms import ProdutoModelForms
@@ -8,4 +9,25 @@ class HomeView(TemplateView):
 
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if str(request.method) == 'POST':
+        form = ProdutoModelForms(request.POST, request.FILES)
+        if form.is_valid():
+            prod = form.save(commit=False)
+
+            print(f'Nome: {prod.nome}')
+            print(f'Preço: {prod.preco}')
+            print(f'Estoque: {prod.estoque}')
+            print(f'Imagem: {prod.imagem}')
+
+            messages.success(request, 'Produto salvo com sucesso')
+        else:
+            messages.error(request, 'Erro ao salvar produto')
+
+    else:
+        form = ProdutoModelForms()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'cadastro.html', context)
